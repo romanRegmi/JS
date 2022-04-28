db3ca26d7869dcb44e93168bef0f2148574f7e21
Roman Regmi <romanregmi@hotmail.com>
2022-04-28 12:42:00 +0545

    Create iterators.js

---
+++ JS_Start/iterators.js
@@ -0,0 +1,41 @@
+function fruitsIterator(values) {
+    let nextIndex = 0;
+    // we will return an object
+    return {
+        next: function () { // The next() method returns an object with two properties done and value.
+            if (nextIndex < values.length) {
+                // We will return the following object
+                return {
+                    value: values[nextIndex++],
+                    done: false
+                }
+            }
+            else {
+                // We will return the following object
+                return {
+                    done: true
+                }
+            }
+        }
+    }
+}
+
+const myArray = ['Apples', 'Grapes', 'Oranges', 'Mango'];
+console.log("My array is ", myArray)
+
+// Using the iterator
+const fruits = fruitsIterator(myArray);
+
+/*
+console.log(fruits.next())
+console.log(fruits.next())
+console.log(fruits.next())
+console.log(fruits.next())
+console.log(fruits.next())
+*/
+
+console.log(fruits.next().value) // Apples
+console.log(fruits.next().value) // Grapes
+console.log(fruits.next().value) // Oranges
+console.log(fruits.next().value) // Mango
+console.log(fruits.next().value) // undefined
