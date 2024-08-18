52750a5ae7014ee56586052338c30d17217fe470
Roman Regmi <romanregmi@hotmail.com>
2024-08-18 13:56:48 +0545

    Added debug

---
+++ JS_Start/debug/debug.js
@@ -0,0 +1,11 @@
+//console.dir(): Displays an object’s properties in an interactive format.
+
+//console.table(): Shows tabular data in a table format.
+
+//console.group(): Creates a collapsible group of log messages.
+
+//console.time(): Starts a timer with a label.
+
+//console.timeEnd(): Stops the timer and logs the elapsed time.
+
+//console.clear(): Clears all messages from the console.
